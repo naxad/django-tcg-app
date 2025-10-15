cf984aeae6f5e3b5acb5e5a8e997d9b7b009af9c
Coedex <tsevastasos@gmail.com>
2025-10-15 13:13:56 +0300

    Prepare for Render deploy

--- .platform/hooks/postdeploy/01_collectstatic.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-echo "[postdeploy] collectstatic"
-source /var/app/venv/*/bin/activate
-python manage.py collectstatic --noinput
--- .platform/hooks/postdeploy/02_migrate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-echo "[postdeploy] migrate"
-source /var/app/venv/*/bin/activate
-python manage.py migrate --noinput
